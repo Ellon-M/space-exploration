86b8877506a48235ec96816c718cf4cf3e51ce80
Donald Akite <donaldakite27@gmail.com>
2023-02-01 07:49:33 +0000

    Add Table for mission display

--- src/components/MissionList.js
+++ src/components/MissionList.js
@@ -1,29 +1,38 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { FetchMIssions } from '../redux/Missions';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FetchMissions } from '../redux/Missions';
 import Missions from './Missions';
 
 function MissionList() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(FetchMIssions());
+    dispatch(FetchMissions());
   }, [dispatch]);
 
-  const listMissions = useSelector((state) => console.log(state));
+  const listMissions = useSelector((mission) => mission.missions.FetchMissions);
+  console.log(listMissions);
 
   return (
-    <>
-      <ul>
-        {listMissions.map((mission) => (
-          <li key={mission.id}>
-            {' '}
-            <Missions name={mission} />
-          </li>
+    <Table striped bordered hover size="xl">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th colSpan={5}>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <thead>
+        { listMissions && listMissions.map((mission) => (
+          <tr key={mission.id}>
+            <Missions mission={mission} />
+          </tr>
         ))}
+      </thead>
 
-      </ul>
-    </>
+    </Table>
   );
 }
 
